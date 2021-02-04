9cf08ab78cb2070d41d0c85fa3c8fb7950d35a61
joshsrai <joshsrai@gmail.com>
2021-02-04 18:31:46 -0500

    commit tree e952d95d201d4b54fa59c797f3cda0494e9c8ae7

--- index.js
+++ index.js
@@ -1,1 +1,62 @@
 // Your code here
+
+function mapToNegativize(arr){
+    let ans = []
+    for (let i = 0; i < arr.length; i++){
+        ans.push(arr[i] *- 1)
+    }
+    return ans
+}
+
+function mapToNoChange(arr){
+    let ans = []
+    for (let i = 0; i < arr.length; i++) {
+        ans.push(arr[i])
+    }
+    return ans
+}
+
+function mapToDouble(arr){
+    let ans = []
+    for (let i = 0; i < arr.length; i++) {
+        ans.push(arr[i] * 2 )
+    }
+    return ans
+}
+
+function mapToSquare(arr) {
+    let ans = []
+    for (let i = 0; i < arr.length; i++) {
+        ans.push(arr[i] ** 2)
+    }
+    return ans
+}
+
+
+function reduceToTotal(arr, startPoiint = 0) {
+    let ans = startPoiint
+    for (let i = 0; i < arr.length; i++) {
+        ans += arr[i]
+    }
+    return ans
+}
+
+function reduceToAllTrue(arr){
+    let ans = true
+    for (let i = 0; i < arr.length; i++ ){
+        if (arr[i] === false){
+            return ans = false
+        }
+    }
+    return ans
+}
+
+function reduceToAnyTrue(arr){
+    let ans = false
+    for (let i = 0; i < arr.length; i++){
+        if (arr[i] === true){
+            return ans = true
+        }
+    }
+    return ans
+}
